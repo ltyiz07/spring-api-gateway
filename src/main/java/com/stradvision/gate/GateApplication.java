c871f12cd779b3709f7681195c06b4c7ca5257c4
Tailor <taekyoung.lee@stradvision.com>
2022-12-19 17:55:33 +0900

    Add router and mongoDB connection

--- src/main/java/com/stradvision/gate/GateApplication.java
+++ src/main/java/com/stradvision/gate/GateApplication.java
@@ -1,13 +1,33 @@
 package com.stradvision.gate;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GateApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r
+                        .path("/**")
+                        .filters(f -> f
+                                .addRequestHeader("role", "tester_role")
+                                .rewritePath("/tester/", "/test/")
+                        )
+                        .uri("http://10.10.10.180:3031")
+                )
+                .build();
+    }
 
 }
